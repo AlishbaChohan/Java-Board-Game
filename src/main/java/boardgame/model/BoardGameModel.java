fb17ccab878dcf5ded8b0bdf119104b0c1f016e9
Alishba Jalil Chohan <alishba.chohana@mailbox.unideb.hu>
2024-05-29 14:00:58 +0200

    feat: implement the isLegalToMoveFrom, isLegalMove and makeMove methods

--- src/main/java/boardgame/model/BoardGameModel.java
+++ src/main/java/boardgame/model/BoardGameModel.java
@@ -1,22 +1,123 @@
 package boardgame.model;
 
 import game.TwoPhaseMoveState;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 
 public class BoardGameModel implements TwoPhaseMoveState<Position> {
+
+    public static final int BOARD_SIZE = 5;
+
+    private final ReadOnlyObjectWrapper<Square>[][] board;
+
+    //for later use
+   // private final ReadOnlyIntegerWrapper numberOfMoves;
+
+    public BoardGameModel() {
+        board = new ReadOnlyObjectWrapper[BOARD_SIZE][BOARD_SIZE-1];
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            for (var j = 0; j < (BOARD_SIZE - 1); j++) {
+                board[i][j] = new ReadOnlyObjectWrapper<>(
+                        switch (i) {
+                            case 0 -> (j % 2 == 0) ? Square.BLUE : Square.RED;
+                            case BOARD_SIZE - 1 -> (j % 2 == 0) ? Square.BLUE : Square.RED;
+                            default -> Square.NONE;
+                        }
+                );
+            }
+        }
+
+        //numberOfMoves = new ReadOnlyIntegerWrapper(0);
+
+
+    }
+
+    //for later use
+//    public ReadOnlyIntegerProperty numberOfMovesProperty() {
+//        return numberOfMoves.getReadOnlyProperty();
+//    }
+
+    public ReadOnlyObjectProperty<Square> squareProperty(int row, int col) {
+        return board[row][col].getReadOnlyProperty();
+    }
+
+    public Square getSquare(Position p) {
+        return board[p.row()][p.col()].get();
+    }
+
+    private void setSquare(Position p, Square square) {
+        board[p.row()][p.col()].set(square);
+    }
+
+    public boolean isEmpty(Position p) {
+        return getSquare(p) == Square.NONE;
+    }
+
+    public static boolean isOnBoard(Position p) {
+        return 0 <= p.row() && p.row() < BOARD_SIZE && 0 <= p.col() && p.col() < BOARD_SIZE;
+    }
+
+
+    public static boolean isKingMove(Position from, Position to) {
+        var dx = Math.abs(to.row() - from.row());
+        var dy = Math.abs(to.col() - from.col());
+        return dx + dy == 1 ;
+    }
+
+
     @Override
-    public boolean isLegalToMoveFrom(Position position) {
-        return false;
+    public boolean isLegalToMoveFrom(Position from) {
+        return isOnBoard(from) && !isEmpty(from);
     }
 
     @Override
-    public boolean isLegalMove(Position position, Position t1) {
-        return false;
+    public boolean isLegalMove(Position from, Position to) {
+        return isLegalToMoveFrom(from) && isOnBoard(to) && isEmpty(to) && isKingMove(from, to);
     }
 
     @Override
-    public void makeMove(Position position, Position t1) {
-
+    public void makeMove(Position from, Position to) {
+        setSquare(to, getSquare(from));
+        setSquare(from, Square.NONE);
+        //numberOfMoves.set(numberOfMoves.get() +  1);
     }
+
+
+
+    //Understand and use for movement
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            for (var j = 0; j < (BOARD_SIZE - 1); j++) {
+                sb.append(board[i][j].get().ordinal()).append(' ');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        var model = new BoardGameModel();
+        System.out.println(model);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public Player getNextPlayer() {
